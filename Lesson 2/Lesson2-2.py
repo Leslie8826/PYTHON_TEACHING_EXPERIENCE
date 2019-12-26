7e549973aaa811c70e846eac7bfbdb6b8f14878b
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-26 16:58:22 -0500

    Update Lesson2-2.py

--- Lesson 2/Lesson2-2.py
+++ Lesson 2/Lesson2-2.py
@@ -22,3 +22,22 @@ print(x, "*", y, "=", x*y)
 print("Division: ")
 print(x, "/", y, "=", x/y)
 
+
+
+##################
+##### Output #####
+##################
+
+# Enter the first number: 12
+# Enter the second number: 5
+
+
+# Arithmetic operations: 
+# Addition: 
+# 12 + 5 = 17
+# Subtraction: 
+# 12 - 5 = 7
+# Multiplication: 
+# 12 * 5 = 60
+# Division: 
+# 12 / 5 = 2.4
