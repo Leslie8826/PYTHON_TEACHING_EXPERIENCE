b55818a84b990f0176d7b04d57d6778b8833bb8b
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-26 17:47:46 -0500

    Update Lesson4.py

--- Lesson 4/Lesson4.py
+++ Lesson 4/Lesson4.py
@@ -1,1 +1,14 @@
+# Create a variable called message and assign a string to this variable
+# Finally, display the content of the variable
+
+message = 'And now for something completely different'
+print(message)
+
+
+##################
+##### Output #####
+##################
+
+# And now for something completely different
+
 
