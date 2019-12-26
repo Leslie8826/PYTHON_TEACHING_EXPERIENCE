fd39a3be21f880234ddf8b6f3e68b5e594bcbdd0
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-26 16:41:01 -0500

    Update Lesson2.py

--- Lesson 2/Lesson2.py
+++ Lesson 2/Lesson2.py
@@ -1,1 +1,24 @@
 
+#The program has to ask a user to type 2 numbers
+#Then the program will display the results of
+# an addition, a substraction, a multiplication and a division
+#between the 2 numbers
+
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+print("\n")
+
+print("Arithmetic operations: ")
+
+print ("Addition: ")
+print(x, "+", y, "=", x+y)
+
+print("Subtraction: ")
+print(x, "-", y, "=", x-y)
+
+print("Multiplication: ")
+print(x, "*", y, "=", x*y)
+
+print("Division: ")
+print(x, "/", y, "=", x/y)
+
