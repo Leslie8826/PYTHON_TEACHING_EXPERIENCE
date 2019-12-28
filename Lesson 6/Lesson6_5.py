8dc3ca6897df4ba8480b0badd7fec5e28d37590d
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-27 23:26:10 -0500

    Update Lesson6_5.py

--- Lesson 6/Lesson6_5.py
+++ Lesson 6/Lesson6_5.py
@@ -1,1 +1,23 @@
+# This program asks the user to enter a temperature.
+# Then the program checks the value entered by the user
+# and print a message according to the value of the temperature entered
 
+temperature = int(input("What is the temperature today? "))
+
+if (temperature > 0):
+  print('This is a great day')
+else:
+  print('Stay home')
+
+  
+
+##################
+##### Output #####
+##################
+#output 1
+What is the temperature today? -25
+Stay home
+
+#output 2
+What is the temperature today? 95
+This is a great day
