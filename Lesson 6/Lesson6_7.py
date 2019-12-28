b5831b78d47ebb7bbdcb2863aeab43bafe64e0ad
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-27 23:52:26 -0500

    Create Lesson6_7.py

---
+++ Lesson 6/Lesson6_7.py
@@ -0,0 +1,19 @@
+# This program compares 2 numbers and print a message
+# using a nested conditional
+
+x=101
+y=11
+
+if x == y:
+  print('x and y are equal')
+else:
+  if x < y:
+    print('x is less than y')
+  else:
+    print('x is greater than y')
+    
+    
+##################
+##### Output #####
+##################
+x is greater than y
