7619c0216f0b37ea3a35f3e3371b341c8d45edf2
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-27 23:46:15 -0500

    Update Lesson6_6.py

--- Lesson 6/Lesson6_6.py
+++ Lesson 6/Lesson6_6.py
@@ -1,6 +1,19 @@
-if (x < y):
-print('x is less than y')
-        elif x > y:
-               print('x is greater than y')
-       else:
-               print('x and y are equal')
+# This program compares 2 numbers and print a message
+# using a chained conditional
+
+x=5
+y=5
+
+if x < y:
+  print('x is less than y')
+elif x > y:
+  print('x is greater than y')
+else:
+  print('x and y are equal')
+
+
+##################
+##### Output #####
+##################
+x and y are equal
+
