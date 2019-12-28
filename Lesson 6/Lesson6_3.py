8fbe2c52396209f10a775907707fe42c456452e8
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-27 22:45:42 -0500

    Update Lesson6_3.py

--- Lesson 6/Lesson6_3.py
+++ Lesson 6/Lesson6_3.py
@@ -1,1 +1,18 @@
+# This program checks if the value of x is positive
+# or negative
 
+x = -10
+
+if x>0:
+  print('x is positive')
+
+else:
+  print('x is negative')
+  
+  
+  
+##################
+##### Output #####
+##################
+
+x is negative
