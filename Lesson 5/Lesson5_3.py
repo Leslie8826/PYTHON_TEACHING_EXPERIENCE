1c01bada867a35785f2ca3eab95977c1922d13b6
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-27 21:19:54 -0500

    Create Lesson5_3.py

---
+++ Lesson 5/Lesson5_3.py
@@ -0,0 +1,19 @@
+# float converts integers and strings to floating-point numbers
+
+# EXAMPLE 1
+float(32)
+
+##################
+##### Output #####
+##################
+32.0
+
+
+
+# EXAMPLE 2
+float('3.14159')
+
+##################
+##### Output #####
+##################
+3.14159
