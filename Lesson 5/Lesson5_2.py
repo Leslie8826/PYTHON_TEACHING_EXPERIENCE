2885e34f0995e0248fd5b0c012e4cd036056183f
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-27 21:13:46 -0500

    Update Lesson5_2.py

--- Lesson 5/Lesson5_2.py
+++ Lesson 5/Lesson5_2.py
@@ -1,1 +1,11 @@
+# The built-in function int converts floating-point values to integers, 
+# but it doesn’t round off; it chops off the fraction part
 
+int(3.99999)
+
+
+##################
+##### Output #####
+##################
+
+3
