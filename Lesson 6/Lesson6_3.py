e4dac506ad2dcc07fc0d435cd262f90fd8ca226f
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-27 23:06:48 -0500

    Update Lesson6_3.py

--- Lesson 6/Lesson6_3.py
+++ Lesson 6/Lesson6_3.py
@@ -1,1 +1,32 @@
+# This program prints if the value of x is positive
+# or negative
 
+# EXAMPLE 1
+x = -10
+
+if x>0:
+  print('This is a great day')
+  
+##################
+##### Output #####
+##################
+
+
+
+
+
+
+
+# EXAMPLE 2
+
+x = -10
+
+if x>0:
+  print('This is a great day')
+else:
+  print('Stay home')
+
+##################
+##### Output #####
+##################
+Stay home
