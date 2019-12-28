3ebedaca6fbe33db4ef78d4250155b1ec281530f
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-27 21:32:38 -0500

    Create Lesson5_4.py

---
+++ Lesson 5/Lesson5_4.py
@@ -0,0 +1,25 @@
+# This function takes an argument 
+# and print twice the argument
+
+
+def print_twice(x):
+  print(x)
+  print(x)
+  
+x='world cup'
+y=7
+
+print_twice(x)
+print_twice(y)
+
+
+
+
+##################
+##### Output #####
+##################
+
+world cup
+world cup
+7
+7
