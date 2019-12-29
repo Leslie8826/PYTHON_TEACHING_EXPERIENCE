6e4d048561d662034ec3363ffc63203206a80a5d
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-28 23:04:26 -0500

    Update Lesson7_3.py

--- Lesson 7/Lesson7_3.py
+++ Lesson 7/Lesson7_3.py
@@ -1,1 +1,27 @@
+# This program asks the user to enter 3 grades.
+# Then the program will average those 3 grades and 
+# display them to the user
 
+a = float(input("Your 1st grade: "))
+b = float(input("Your 2nd grade: "))
+c = float(input("Your 3rd grade: "))
+
+def average(a, b, c):
+  return(a+b+c)/3
+
+print() # this is a space
+print("average = ", end="")
+average(a, b, c)
+
+
+
+
+##################
+##### Output #####
+##################
+
+Your 1st grade: 95.7
+Your 2nd grade: 99.3
+Your 3rd grade: 92.5
+
+average = 95.83333333333333
