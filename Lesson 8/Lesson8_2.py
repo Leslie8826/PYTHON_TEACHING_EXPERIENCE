334a7aa6839819523e0d9571d4a163408f9c6982
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-28 23:46:36 -0500

    Update Lesson8_2.py

--- Lesson 8/Lesson8_2.py
+++ Lesson 8/Lesson8_2.py
@@ -1,4 +1,5 @@
-# This program
+# This program asks the user to enter a message using a while loop
+#  and uses a break statement (in this case "done") to end the loop
 
 while True: 
   line = input('> ') 
@@ -6,3 +7,21 @@ while True:
     break
   print(line)
 print('Done!') 
+
+
+
+
+##################
+##### Output #####
+##################
+> this is awesome
+this is awesome
+> yeaaaahhhhh
+yeaaaahhhhh
+> test
+test
+> test 2
+test 2
+> done
+Done!
+
