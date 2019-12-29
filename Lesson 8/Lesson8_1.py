d3ea81dcd3e51e3705c573ff5d237e904f44a5b6
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-28 23:37:03 -0500

    Update Lesson8_1.py

--- Lesson 8/Lesson8_1.py
+++ Lesson 8/Lesson8_1.py
@@ -1,1 +1,24 @@
+# This program countdowns one by one 
+# from a number n using a while loop. 
+# Once n reaches a certain value
+# the program prints "Blastoff!"
+
+def countdown(n): 
+  while n > 0: 
+    print(n) 
+    n = n - 1 
+  print('Blastoff!') 
+  
+countdown(3)
+
+
+
+##################
+##### Output #####
+##################
+
+3
+2
+1
+Blastoff!
 
