9d4d7fa17d4b5e5d1c0c679c033f9aed156ec2fb
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-30 16:08:44 -0500

    Update Lesson11_1.py

--- Lesson 11/Lesson11_1.py
+++ Lesson 11/Lesson11_1.py
@@ -1,1 +1,11 @@
+# This program 
+d = dict()
+print("dictionary d at first: \n", d)
 
+d = {'one': 'uno', 'two': 'dos', 'three': 'tres'}
+print("\n dictionary d after adding items \n", d)
+print("\n Element at indice two: \n", d['two'])
+
+##################
+##### Output #####
+##################
