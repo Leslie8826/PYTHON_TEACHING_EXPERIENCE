805e812d9b2f4f9958c116f9fc5d9865e842cbee
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-30 16:45:02 -0500

    Update Lesson12_1.py

--- Lesson 12/Lesson12_1.py
+++ Lesson 12/Lesson12_1.py
@@ -1,1 +1,30 @@
+# This program creates 2 tuples and swipe them
+t = ('a', 'b', 'c')
+p = ('v', 'x', 'w')
 
+print('BEFORE SWAPPING:')
+print('t = ', end='')
+print(t)
+print('p = ', end='')
+print(p)
+
+t, p = p, t
+print('\n AFTER SWAPPING:')
+print('t = ', end='')
+print(t)
+print('p = ', end='')
+print(p)
+
+
+
+
+##################
+##### Output #####
+##################
+BEFORE SWAPPING:
+t = ('a', 'b', 'c')
+p = ('v', 'x', 'w')
+
+ AFTER SWAPPING:
+t = ('v', 'x', 'w')
+p = ('a', 'b', 'c')
