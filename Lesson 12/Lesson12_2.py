a6c0f29d0050d159fa8221a4574f358c1e907119
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-30 17:08:39 -0500

    Update Lesson12_2.py

--- Lesson 12/Lesson12_2.py
+++ Lesson 12/Lesson12_2.py
@@ -1,12 +1,12 @@
-# This program 
+# This program creates 3 tuples. It proceeds and 
+# displays a few operations about the 3 tuples
 tup1 = ('physics', 'chemistry', 2015, 2018)
 tup2 = (1, 2, 3, 4, 5)
 tup3 = "a", "b", "c", "d"
 
-print("tup1[0] = ", tup1[0])
-print("tup2[1:3] = ", tup2[1:3])
-print("tup3 = ", tup3)
-
+print("tup1[0] = ", tup1[0]) # display the tuple element at index 0
+print("tup2[1:3] = ", tup2[1:3]) # display the element from index 1 to index 2
+print("tup3 = ", tup3) # display the entire tuple tup3
 
 
 ##################
