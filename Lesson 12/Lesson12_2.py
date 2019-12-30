31a9b7b8f725f5f0f265dad4ff9d4aa8a30c3e35
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-30 17:00:28 -0500

    Update Lesson12_2.py

--- Lesson 12/Lesson12_2.py
+++ Lesson 12/Lesson12_2.py
@@ -1,1 +1,17 @@
+# This program 
+tup1 = ('physics', 'chemistry', 2015, 2018)
+tup2 = (1, 2, 3, 4, 5)
+tup3 = "a", "b", "c", "d"
 
+print("tup1[0] = ", tup1[0])
+print("tup2[1:3] = ", tup2[1:3])
+print("tup3 = ", tup3)
+
+
+
+##################
+##### Output #####
+##################
+tup1[0] =  physics
+tup2[1:3] =  (2, 3)
+tup3 =  ('a', 'b', 'c', 'd')
