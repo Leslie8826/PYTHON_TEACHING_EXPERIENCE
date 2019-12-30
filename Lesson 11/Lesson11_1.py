bf97f0c8d73f4c5629e3e3e9093e70cd8b2d04b5
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-30 16:09:51 -0500

    Update Lesson11_1.py

--- Lesson 11/Lesson11_1.py
+++ Lesson 11/Lesson11_1.py
@@ -1,4 +1,5 @@
-# This program 
+# This program creates an empty dictionary
+# and add items to it
 d = dict()
 print("dictionary d at first: \n", d)
 
@@ -6,6 +7,16 @@ d = {'one': 'uno', 'two': 'dos', 'three': 'tres'}
 print("\n dictionary d after adding items \n", d)
 print("\n Element at indice two: \n", d['two'])
 
+
+
 ##################
 ##### Output #####
 ##################
+dictionary d at first: 
+ {}
+
+ dictionary d after adding items 
+ {'one': 'uno', 'two': 'dos', 'three': 'tres'}
+
+ Element at indice two: 
+ dos
