06b1454fa57e59a7d860e471eeccd982d3ab1a2e
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-29 19:58:54 -0500

    Update Lesson9_1.py

--- Lesson 9/Lesson9_1.py
+++ Lesson 9/Lesson9_1.py
@@ -1,1 +1,12 @@
+# This program calculates the length of a string
+fruit = 'strawberry'
 
+print('Number of letters in', fruit, 'is: ', end='')
+len(fruit)
+
+
+
+##################
+##### Output #####
+##################
+Number of letters in strawberry is: 10
