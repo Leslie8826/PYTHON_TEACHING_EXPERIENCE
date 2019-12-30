84c5cfda8ffb1c16d928074aa09102638b44199e
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-29 20:21:51 -0500

    Create Lesson9_4.py

---
+++ Lesson 9/Lesson9_4.py
@@ -0,0 +1,32 @@
+# Slice 1
+# name_of_string[:n]
+# This program slices a string. It returns the part of the string 
+# that is before the the “n-eth” character including the “n-eth” character
+
+fruit = 'watermelon'
+
+fruit[:5]
+
+
+##################
+##### Output #####
+##################
+'water'
+
+
+
+
+
+# Slice 2
+# name_of_string[m:]
+# This program slices a string. It returns the part of the string 
+# that is after the the “m-eth” character including the “m-eth” character
+
+fruit = 'watermelon'
+
+fruit[5:]
+
+##################
+##### Output #####
+##################
+'melon'
