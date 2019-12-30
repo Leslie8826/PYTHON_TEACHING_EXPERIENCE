68173f4746d8d6e174a2acc30dbb9cbdb1aeaa4c
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-29 22:49:29 -0500

    Update Lesson10_4.py

--- Lesson 10/Lesson10_4.py
+++ Lesson 10/Lesson10_4.py
@@ -1,1 +1,19 @@
+# This program traverses a list of numbers and prints each element
+numbers = [8, 7, 55, 6, -1000, 444, 698]
 
+for i in numbers:
+  print(i)
+  
+  
+  
+  
+##################
+##### Output #####
+##################
+8
+7
+55
+6
+-1000
+444
+698
