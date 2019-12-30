291dafa20fbbb748554827ecf350ce4ed27ee397
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-29 23:14:29 -0500

    Update Lesson10_5.py

--- Lesson 10/Lesson10_5.py
+++ Lesson 10/Lesson10_5.py
@@ -1,1 +1,28 @@
+# This program creates 2 lists and makes operation on them
+a = [1, 2, 3]
+b = [4, 5, 6]
+print("List a: ", a)
+print("List b: ", b)
 
+print("\n OPERATIONS ON LISTS: ")
+print("a + b :")
+c = a + b
+print(c)
+
+print("\n a * 3: ")
+d = a * 3
+print(d)
+
+
+##################
+##### Output #####
+##################
+List a:  [1, 2, 3]
+List b:  [4, 5, 6]
+
+ OPERATIONS ON LISTS: 
+a + b :
+[1, 2, 3, 4, 5, 6]
+
+ a * 3: 
+[1, 2, 3, 1, 2, 3, 1, 2, 3]
