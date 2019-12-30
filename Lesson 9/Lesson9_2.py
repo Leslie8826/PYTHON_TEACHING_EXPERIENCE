ed53b48f037374769f24616a2b074022838db797
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-29 20:04:09 -0500

    Update Lesson9_2.py

--- Lesson 9/Lesson9_2.py
+++ Lesson 9/Lesson9_2.py
@@ -1,1 +1,20 @@
+# This program traverses a string 
+# with a while loop
 
+fruit = 'apple'
+
+index = 0
+while index < len(fruit):
+  letter = fruit[index]
+  print(letter) 
+  index = index + 1
+  
+  
+##################
+##### Output #####
+##################
+a
+p
+p
+l
+e
