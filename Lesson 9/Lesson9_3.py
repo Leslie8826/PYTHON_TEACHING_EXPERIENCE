3ad0dccbbf8103523a48f76b7974734eaef7f486
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-29 20:16:36 -0500

    Update Lesson9_3.py

--- Lesson 9/Lesson9_3.py
+++ Lesson 9/Lesson9_3.py
@@ -1,4 +1,7 @@
-# This program slices a string
+# name_of_string[n:m]
+# This program slices a string. It returns the part of the string from the “n-eth” character
+#  to the “m-eth” character including the first but excluding the last
+
 s = 'Monty Python'
 
 s[3:9]
