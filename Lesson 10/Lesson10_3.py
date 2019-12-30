dd94e9cc7dd7a8f0d35aa9511aac3f353198e21f
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-29 22:18:38 -0500

    Update Lesson10_3.py

--- Lesson 10/Lesson10_3.py
+++ Lesson 10/Lesson10_3.py
@@ -1,1 +1,34 @@
+# This program creates a list with 2 elements
+# and replaces one of the 2 elements with a new element.
+# It prints the new list.
+# The program also checks if an element is in the list.
 
+numbers = [42, 105]
+
+print('List')
+print('numbers = ', end="")
+print(numbers)
+print('\n')
+
+print('List after changing second value: ')
+numbers[1] = -3
+print('numbers = ', end='')
+print(numbers)
+print('\n')
+
+print('Is 8 a value of the list?')
+8 in numbers
+
+
+
+##################
+##### Output #####
+##################
+List
+numbers = [42, 105]
+
+List after changing second value: 
+numbers = [42, -3]
+
+Is 8 a value of the list?
+False
