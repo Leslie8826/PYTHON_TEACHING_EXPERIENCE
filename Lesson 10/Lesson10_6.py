60f8c8937b41214c13dccb9a50752fcf17679d1a
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-29 23:24:37 -0500

    Update Lesson10_6.py

--- Lesson 10/Lesson10_6.py
+++ Lesson 10/Lesson10_6.py
@@ -1,1 +1,17 @@
+# name_of_string[n:m]
+# This program slices a list. It returns the part of the list from the “n-eth” character
+#  to the “m-eth” character including the first but excluding the last
+t = ['a', 'b', 'c', 'd', 'e', 'f']
+print("List t: ", t)
+print("\n Slice t[1:3] results in: ")
+t[1:3]
+
+##################
+##### Output #####
+##################
+List t:  ['a', 'b', 'c', 'd', 'e', 'f']
+
+ Slice t[1:3] results in: 
+['b', 'c']
+
 
