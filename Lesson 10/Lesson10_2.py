2a38605e3ce2398225650eb9a0693c55b424dc99
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-29 22:11:28 -0500

    Update Lesson10_2.py

--- Lesson 10/Lesson10_2.py
+++ Lesson 10/Lesson10_2.py
@@ -1,1 +1,12 @@
+# This programs displays the first element of a list
+cheeses = ['Cheddar', 'Edam', 'Gouda']
 
+first_element = cheeses[0]
+
+print("The first element of the list is: ", first_element)
+
+
+##################
+##### Output #####
+##################
+The first element of the list is:  Cheddar
