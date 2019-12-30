bae979fb3441c86f6accc7cbe615175306bbeb07
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-29 23:52:13 -0500

    Update Lesson10_7.py

--- Lesson 10/Lesson10_7.py
+++ Lesson 10/Lesson10_7.py
@@ -1,1 +1,19 @@
 
+t = ['a', 'b', 'c']
+print("List: \n", t)
+x = t.pop(1)
+print("\n List after pop operation on element 1: \n", t)
+print ("\n Element 1 removed from list: \n", x)
+
+
+##################
+##### Output #####
+##################
+List: 
+ ['a', 'b', 'c']
+
+ List after pop operation on element 1: 
+ ['a', 'c']
+
+ Element 1 removed from list: 
+ b
