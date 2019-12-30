5a0c58cdd3ba41a66d6a9e4d12fb974ebd378ffe
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-29 22:06:32 -0500

    Update Lesson10_1.py

--- Lesson 10/Lesson10_1.py
+++ Lesson 10/Lesson10_1.py
@@ -1,1 +1,12 @@
+# This program prints 3 lists
+cheeses = ['Cheddar', 'Edam', 'Gouda']
+numbers = [42, 123]
+empty = []
 
+print(cheeses, numbers, empty)
+
+
+##################
+##### Output #####
+##################
+['Cheddar', 'Edam', 'Gouda'] [42, 123] []
