2e8223086dc78dc77224f87ecf3f32984f1239b7
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-29 23:30:52 -0500

    Update Lesson10_6.py

--- Lesson 10/Lesson10_6.py
+++ Lesson 10/Lesson10_6.py
@@ -1,3 +1,4 @@
+# SLICE 1
 # name_of_string[n:m]
 # This program slices a list. It returns the part of the list from the “n-eth” character
 #  to the “m-eth” character including the first but excluding the last
@@ -15,3 +16,29 @@ List t:  ['a', 'b', 'c', 'd', 'e', 'f']
 ['b', 'c']
 
 
+
+
+# SLICE 2
+# name_of_string[:n]
+# This program slices a list. It returns the part of the list 
+# that is before the the “n-eth” character including the “n-eth” character
+t = ['a', 'b', 'c', 'd', 'e', 'f']
+print("List t: ", t)
+print("\n Slice t[:4] results in: ")
+t[:4]
+
+##################
+##### Output #####
+##################
+List t:  ['a', 'b', 'c', 'd', 'e', 'f']
+
+ Slice t[:4] results in: 
+['a', 'b', 'c', 'd']
+
+
+
+
+# SLICE 3
+# name_of_string[m:]
+# This program slices a list. It returns the part of the list 
+# that is after the the “m-eth” character including the “m-eth” character
