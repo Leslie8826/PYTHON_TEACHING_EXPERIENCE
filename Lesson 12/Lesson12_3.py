c6b2cecb58c366d1c4c367c9b5d9b9158395d0be
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-30 17:18:28 -0500

    Update Lesson12_3.py

--- Lesson 12/Lesson12_3.py
+++ Lesson 12/Lesson12_3.py
@@ -1,1 +1,14 @@
+# This program takes portions of existing tuples to create new tuples 
+tup1 = (12, 34.56)
+tup2 = ('abc', 'xyz')
 
+tup3 = tup1 + tup2
+
+print(tup3)
+
+
+
+##################
+##### Output #####
+##################
+(12, 34.56, 'abc', 'xyz')
