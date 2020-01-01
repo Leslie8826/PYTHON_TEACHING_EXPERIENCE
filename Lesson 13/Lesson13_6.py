748cac8488410a68ef9f12170b2de29d9b070695
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-01-01 16:44:18 -0500

    Update Lesson13_6.py

--- Lesson 13/Lesson13_6.py
+++ Lesson 13/Lesson13_6.py
@@ -1,1 +1,44 @@
 
+import numpy as np
+
+# Creates a rank 2 array with shape (3, 4)
+a = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+print("Array a: \n", a)
+
+# Mixing integer with slices to access rows or columns of an array
+
+# Displays the second row (row 1) of array a in two ways
+print("\n")
+row_r1 = a[1, :]    
+row_r2 = a[1:2, :]  
+print(row_r1, row_r1.shape)  
+print(row_r2, row_r2.shape)  
+
+print("\n")  #add space between lines
+
+# Displays column 1 of array a in two ways
+col_r1 = a[:, 1]
+col_r2 = a[:, 1:2]
+print(col_r1, col_r1.shape) 
+print(col_r2, col_r2.shape) 
+
+
+
+
+##################
+##### Output #####
+##################
+Array a: 
+ [[ 1  2  3  4]
+ [ 5  6  7  8]
+ [ 9 10 11 12]]
+
+
+[5 6 7 8] (4,)
+[[5 6 7 8]] (1, 4)
+
+
+[ 2  6 10] (3,)
+[[ 2]
+ [ 6]
+ [10]] (3, 1)
