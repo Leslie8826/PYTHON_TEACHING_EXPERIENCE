407e3622cb09e03710cf0b9db8263cd7563112c4
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-31 22:52:08 -0500

    Update Lesson13_5.py

--- Lesson 13/Lesson13_5.py
+++ Lesson 13/Lesson13_5.py
@@ -1,1 +1,9 @@
+# TO BE CONTINUED...
+import numpy as np
 
+# Create the following rank 2 array with shape (3, 4)
+# [[ 1  2  3  4]
+#  [ 5  6  7  8]
+#  [ 9 10 11 12]]
+a = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+print("Matrix a: \n", a)
