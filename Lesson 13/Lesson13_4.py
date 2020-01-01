28ac41f24bd320a3f8ab987cc60e300e11e32a8d
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2019-12-31 22:39:19 -0500

    Update Lesson13_4.py

--- Lesson 13/Lesson13_4.py
+++ Lesson 13/Lesson13_4.py
@@ -1,16 +1,16 @@
-# This programs creates an array of rank 2
+# This programs creates an array of rank 1
 # filled with all ones
 
 import numpy as np
 
-# Create an 2-rank array of all ones
+# Create an 1-rank array of all ones
 b = np.ones((2,3))    
-print("2-rank array of all ones: \n", b)  
+print("1-rank array of all ones: \n", b)  
 
 
 ##################
 ##### Output #####
 ##################
-2-rank array of all ones: 
+1-rank array of all ones: 
  [[1. 1. 1.]
  [1. 1. 1.]]
