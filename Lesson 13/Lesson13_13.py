dd82acff0ba6e27b134b4c4e9919ea6eca1248c7
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-01-01 20:47:33 -0500

    Update Lesson13_13.py

--- Lesson 13/Lesson13_13.py
+++ Lesson 13/Lesson13_13.py
@@ -1,1 +1,35 @@
+# This program displays the transpose
+# of matrices of different ranks
 
+import numpy as np
+
+x = np.array([[1,2], [3,4]])
+print("Array x:\n", x)
+
+print("\n Transpose of array x: \n", x.T) 
+
+# Taking the transpose of a rank 1 array does nothing
+v = np.array([1,2,3])
+print("\n Array v: \n", v)    
+print("\n Transpose of v: \n", v.T)
+
+
+
+
+
+##################
+##### Output #####
+##################
+Array x:
+ [[1 2]
+ [3 4]]
+
+Transpose of array x: 
+ [[1 3]
+ [2 4]]
+
+Array v: 
+ [1 2 3]
+
+Transpose of v: 
+ [1 2 3]
