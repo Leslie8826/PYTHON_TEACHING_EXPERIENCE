dd72520a30f712c8e825ebc36fe899c768be5350
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-01-01 21:31:26 -0500

    Update Lesson13_16.py

--- Lesson 13/Lesson13_16/Lesson13_16.py
+++ Lesson 13/Lesson13_16/Lesson13_16.py
@@ -2,8 +2,17 @@
 # It chooses a certain type of plots to display the points
 # The x axis goes from 0 to 6
 # The y axis goes from 0 to 20
+
+
 import matplotlib.pyplot as plt
 
 plt.plot([1,2,3,4], [1,4,9,16], 'ro') # coordinates of points
 plt.axis([0, 6, 0, 20]) # axis dimensions
 plt.show()
+
+
+
+##################
+##### Output #####
+##################
+# see numpy_matplotlib3.png to visualize the plot
