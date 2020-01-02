3ca56d5ac0cdc725267d821b0ca710cfd8a79670
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-01-01 21:30:50 -0500

    Update Lesson13_16.py

--- Lesson 13/Lesson13_16/Lesson13_16.py
+++ Lesson 13/Lesson13_16/Lesson13_16.py
@@ -1,3 +1,7 @@
+# This program plots points according to their coordinates
+# It chooses a certain type of plots to display the points
+# The x axis goes from 0 to 6
+# The y axis goes from 0 to 20
 import matplotlib.pyplot as plt
 
 plt.plot([1,2,3,4], [1,4,9,16], 'ro') # coordinates of points
