76046534418dafbc87956d54e818d1492bcbcf3d
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-01-01 19:15:26 -0500

    Update Lesson13_9.py

--- Lesson 13/Lesson13_9.py
+++ Lesson 13/Lesson13_9.py
@@ -1,1 +1,33 @@
 
+import numpy as np
+
+x = np.array([1, 2]) # Let numpy choose the datatype
+print("Array x: \n", x)   
+print("Datatype of elements of x: \n", x.dtype)         
+
+y = np.array([1.0, 2.0])   # Let numpy choose the datatype
+print("\n Array y: \n", y)
+print(" Datatype of elements of y: \n", y.dtype)             
+
+z = np.array([1, 2], dtype=np.int64)   # Force a particular datatype
+print("\n Array z: \n", z)
+print("Datatype of elements of z: \n", z.dtype)  
+
+
+##################
+##### Output #####
+##################
+Array x: 
+ [1 2]
+Datatype of elements of x: 
+ int64
+
+ Array y: 
+ [1. 2.]
+ Datatype of elements of y: 
+ float64
+
+ Array z: 
+ [1 2]
+Datatype of elements of z: 
+ int64
