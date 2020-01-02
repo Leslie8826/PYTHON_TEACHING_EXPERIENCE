b483044fb8dfdf8ea6ea5a20d64490235c2baca2
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-01-01 21:07:28 -0500

    Update Lesson13_14.py

--- Lesson 13/Lesson13_14/Lesson13_14.py
+++ Lesson 13/Lesson13_14/Lesson13_14.py
@@ -1,7 +1,14 @@
-# This program plots sin(x) function 
-
+# This program plots sin(x) function for values
+# from 0 to 5 with spacing of 0.1 between values
 import numpy as np
 import matplotlib.pyplot as plt
 x = np.arange(0, 5, 0.1);
 y = np.sin(x)
 plt.plot(x, y)
+
+
+
+##################
+##### Output #####
+##################
+# see numpy_matplot1.png to visualize the plot
