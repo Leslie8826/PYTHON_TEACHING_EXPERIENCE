5222e09bd0d23dc30336aa77a0b4875261cbf643
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-01-25 23:34:19 -0500

    Update Lesson13_16.py

--- Lesson 13/Lesson13_16/Lesson13_16.py
+++ Lesson 13/Lesson13_16/Lesson13_16.py
@@ -3,13 +3,11 @@
 # The x axis goes from 0 to 6
 # The y axis goes from 0 to 20
 
-
 import matplotlib.pyplot as plt
 
 plt.plot([1,2,3,4], [1,4,9,16], 'ro') # coordinates of points
 plt.axis([0, 6, 0, 20]) # axis dimensions
 plt.show()
-
 
 
 ##################
